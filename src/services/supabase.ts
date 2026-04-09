c89ea2c3f4ec53823425322980a6ae4c52444b69
Kurmankul Nurislam <kurn9367@gmail.com>
2026-04-10 02:01:33 +0500

    Clear Supabase credentials
    Removed sensitive Supabase URL and anon key.

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -2,8 +2,8 @@ import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export const SUPABASE_URL = 'https://gadnsdnbflexvggcghji.supabase.co';
-export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdhZG5zZG5iZmxleHZnZ2NnaGppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU2Njk1NTQsImV4cCI6MjA5MTI0NTU1NH0.lZYYOy3PxdJ7Yqlc3KLHiOaWrh3aMyQHp3hpabesQEA';
+export const SUPABASE_URL = '';
+export const SUPABASE_ANON_KEY = '';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
